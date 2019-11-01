a5bf01e6b99128aa656c40dd4981439ba5193798
Eric Kiminza <44212485+kiminzajnr@users.noreply.github.com>
2019-11-01 21:42:57 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello World")
+def helloworld(x):
+	print(x)
+helloworld("Hello World")
+
+def name(y):
+	print("\nMy name is", y, ".")
+	def age():
+		print("I am 20+ years of age. Young, right? \U0001f600")
+	age()
+name("Stan")
+
+sum = 0
+lst = [12, 4, 56, 17, 8, 99]
+for num in lst:
+	sum = sum + num
+	mean = sum / len(lst)
+print("\nThe maximum number in this list:  [12, 4, 56, 17, 8, 99] is", max(lst), ".")
+print("The mean: [12, 4, 56, 17, 8, 99] is", mean)
+
+print("\nA for Apple\nB for Boy\nC for Cow\n...\nZ for Zebra")
